8f3fd7a06e307eb88e3937406df7266ae611588f
Francisco Sarrias <48757758+fsarrias@users.noreply.github.com>
2022-05-09 17:40:21 +0200

    I create the expenses end point

--- pages/api/expenses/[id].js
+++ pages/api/expenses/[id].js
@@ -2,16 +2,16 @@ import prisma from "lib/prisma"
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
-        const trip = await prisma.trip.findUnique({
+        const expense = await prisma.expense.findUnique({
             where: {
                 id: parseInt(req.query.id),
             }
         })
-        if (!trip) {
+        if (!expense) {
             return res.status(404).json({message: 'Not Found'})
         }
 
-        res.status(200).json(trip)
+        res.status(200).json(expense)
     }
     if (req.method ==='PUT') {
 
--- pages/api/expenses/index.js
+++ pages/api/expenses/index.js
@@ -1,35 +1,54 @@
 import prisma from "lib/prisma"
 
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        const trips = await prisma.trip.findMany()
-        res.status(200).json(trips)
-        return
-    }
-    
     if (req.method === 'POST') {
-        const {user, name, start_date, end_date} = req.body
-        if (!user) {
+        const { trip, name, date, amount, currency } = req.body
+        if (!trip) {
             return res
             .status(400)
-            .json({message: 'Missing required parameter user'})
+            .json({message: 'Missing required parameter trip'})
         }
         if (!name) {
             return res
             .status(400)
             .json({message: 'Missing required parameter name'})
         }
-
-        await prisma.trip.create({
+        if (!amount) {
+            return res
+            .status(400)
+            .json({message: 'Missing required parameter amount'})
+        }
+        if (!currency) {
+            return res
+            .status(400)
+            .json({message: 'Missing required parameter currency'})
+        }
+        await prisma.expense.create({
             data: {
-                user,
+                trip,
                 name,
-                start_date,
-                end_date,
+                amount,
+                currency,
             }
         })
         res.status(200).end()
         return
+
+
+
+
+
+
+
+
+
+        res.status(200).json(trips)
+        return
+    }
+    
+    if (req.method === 'POST') {
+        const {user, name, start_date, end_date} = req.body
+       
     }
     
    
